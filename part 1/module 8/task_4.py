f61a156c11751ccd893e295856b0904937a98373
Julia Polyakova <razvlechenia.svoboda@yandex.ru>
2024-04-19 18:21:18 +0500

    add module 8

---
+++ part 1/module 8/task_4.py
@@ -0,0 +1,22 @@
+print('Задача 4. Отрезок')
+
+# Напишите программу, которая считывает с клавиатуры три числа a, b и c, считает и выводит на консоль среднее арифметическое всех чисел из отрезка [a; b], кратных числу c.
+
+first_number = int(input('Введите первое число: '))
+second_number = int(input('Введите второе число: '))
+third_number = int(input('Введите третье число: '))
+
+total = 0
+count = 0
+
+for num in range(first_number, second_number + 1):
+    if num % third_number == 0:
+        total += num
+        count += 1
+
+if count == 0:
+    print(f'Нет чисел, кратных {third_number}')
+else:
+    print(f'Среднее арифметическое всех чисел из отрезка [{first_number}, {second_number}] равно {total / count}')
+
+print()
